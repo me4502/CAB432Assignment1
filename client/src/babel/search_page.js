35b0544bbbf889afb572146741eb08dec2cb4845
Matthew Miller <mnmiller1@me.com>
2018-09-14 12:04:47 +1000

    Push code for work on another computer.

--- client/src/babel/search_page.js
+++ client/src/babel/search_page.js
@@ -7,5 +7,18 @@ function populateImages() {
 
     const search = document.querySelector("#search_bar").value;
 
-
+    for (let url in urls) {
+        const div = `<div class="row">
+            <div class="col s12 m7">
+              <div class="card">
+                <div class="card-image">
+                  <img src="${url}">
+                </div>
+                <div class="card-action">
+                  <a href="#">Choose Image</a>
+                </div>
+              </div>
+            </div>
+          </div>`
+    }
 }
